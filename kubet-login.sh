39c795f0518a2d21025a70c9d1db7e62e5f66b5e
Matthias <oeggge@gmail.com>
2025-08-28 21:32:58 +0200

    login for docker pull secrets

---
+++ kubet-login.sh
@@ -0,0 +1,6 @@
+kubectl create secret docker-registry ghcr-cred \
+  --docker-server=ghcr.io \
+  --docker-username=Oegge \
+  --docker-password= \
+  --docker-email=oeggge@gmail.com
+                                      
