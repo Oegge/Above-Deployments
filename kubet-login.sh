04146c37d31a13bf511dd5d07b29604234a8afe6
Matthias <oeggge@gmail.com>
2026-01-25 23:51:32 +0100

    remove
    deployment

--- kubet-login.sh
+++ kubet-login.sh
@@ -1,5 +1,6 @@
 kubectl create secret docker-registry ghcr-cred \
-  -n above \
+  -n above-prod \
   --docker-server=ghcr.io \
   --docker-username=Oegge \
+  --docker-password= \
   --docker-email=oeggge@gmail.com
